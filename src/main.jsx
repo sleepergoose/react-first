233f3c904be01cecb460c95fba47fcf069358b05
sleepergoose <sleepergoose@gmail.com>
2024-07-06 09:53:26 +0300

    Delete redundunt content

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import Root from './Root.jsx';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+  },
+  {
+    path: '*',
+    element: <Root />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} fallbackElement={<p>Initial Load...</p>} />
   </React.StrictMode>
 );
