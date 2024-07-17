0b7501f1aae23b1b391f42348a9bb4176bbd03ab
sleepergoose <sleepergoose@gmail.com>
2024-07-17 14:27:02 +0300

    Refactor the useLocalStorage hook

--- src/hooks/use-local-storage.jsx
+++ src/hooks/use-local-storage.jsx
@@ -1,27 +1,26 @@
 import { useState, useEffect } from 'react';
 
-const useLocalStorage = (key, initialValue) => {
-  const [value, setValue] = useState(getValue);
-
-  const getValue = () => {
-    try {
-      const _value = localStorage.getItem(key);
-
-      if (_value) {
-        return JSON.parse(_value);
-      }
-
-      return initialValue;
-    } catch (error) {
-      console.log(error);
+const getValue = (key, initialValue) => {
+  try {
+    const _value = localStorage.getItem(key);
+    if (_value) {
+      return JSON.parse(_value);
     }
-  };
+
+    return initialValue;
+  } catch (error) {
+    console.log(error?.message);
+  }
+};
+
+const useLocalStorage = (key, initialValue) => {
+  const [value, setValue] = useState(getValue(key, initialValue || null));
 
   useEffect(() => {
     try {
       localStorage.setItem(key, JSON.stringify(value));
     } catch (error) {
-      console.log(error);
+      console.log(error?.message);
     }
   }, [key, value]);
 
