3840f17ef8c4d73f6b123df86a2a027588f0a473
sleepergoose <sleepergoose@gmail.com>
2024-07-29 15:27:09 +0300

    Provide auth redux data persisting

--- src/store/store.js
+++ src/store/store.js
@@ -3,8 +3,9 @@ import configs from '../configuration/config.js';
 import rootReducer from './root-reducer.js';
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from './sagas/root-saga.js';
+import { getAuthState, saveAuthState } from '../utils/local-storage.utils.js';
 
-const preloadedState = {};
+const preloadedState = getAuthState();
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -16,5 +17,11 @@ const store = configureStore({
 });
 
 sagaMiddleware.run(rootSaga);
+
+store.subscribe(() => {
+  saveAuthState({
+    auth: store.getState().auth,
+  });
+});
 
 export default store;
